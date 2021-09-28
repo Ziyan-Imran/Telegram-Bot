64fdaaafce5abee4a3dbca810faf0a71070f4dd5
Ziyan <ziyan.imran@gmail.com>
2021-09-28 19:59:02 -0400

    Changed order of logging

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
 from typing import Text
 import telegram
-from telegram.ext import Updater, dispatcher
+from telegram.ext import Updater, Dispatcher, CommandHandler, MessageHandler, Filters
 from config import *
-
-bot = telegram.Bot(token=TOKEN)
-print(bot.get_me())
 
 # Create updater object: Continously fetches new updates from telegram and passes them on to the Dispatcher
 updater = Updater(token=TOKEN, use_context=True)
 dispatcher = updater.dispatcher
 
+import logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+bot = telegram.Bot(token=TOKEN)
+print(bot.get_me())
+
 updates = bot.get_updates()
 print(updates[0])
 
-bot.send_message(text="Hi Me again!", chat_id=chatId)
+# Function called when the Bot receievs a certain command
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+def echo(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
+# Bot responds to when user types the '/start/ command
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
+updater.start_polling()
+
+echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
+dispatcher.add_handler(echo_handler)
+
+updater.start_polling()
