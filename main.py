e89688e9b720388cb80ba735537406d7132efdb2
Ziyan <ziyan.imran@gmail.com>
2021-09-28 19:45:46 -0400

    Add updater/dispatcher

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from typing import Text
 import telegram
+from telegram.ext import Updater, dispatcher
 from config import *
 
 bot = telegram.Bot(token=TOKEN)
 print(bot.get_me())
+
+# Create updater object: Continously fetches new updates from telegram and passes them on to the Dispatcher
+updater = Updater(token=TOKEN, use_context=True)
+dispatcher = updater.dispatcher
 
 updates = bot.get_updates()
 print(updates[0])
