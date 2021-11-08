3e034a158e5fdbfc26acf14ffe5437ab3399f900
Ziyan Imran <ziyan.imran@gmail.com>
2021-11-07 19:32:20 -0500

    Added Strikethrough, Italic, Bold&Italic text options

--- main.py
+++ main.py
@@ -9,22 +9,26 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 from typing import Text
 import telegram
 from telegram import InlineQueryResultArticle, InputTextMessageContent, Update
-from telegram.ext import Updater, Dispatcher, CommandHandler, MessageHandler, Filters, InlineQueryHandler, dispatcher, inlinequeryhandler
+from telegram.ext import Updater, Dispatcher, CommandHandler, MessageHandler, Filters, InlineQueryHandler, dispatcher, \
+    inlinequeryhandler
 from capsbot import *
 from timerbot import *
+
 
 # Define a few command handlers
 def start(update: Update, _: CallbackContext) -> None:
     _.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
+
 def error(update: Update, _: CallbackContext) -> None:
     print(f"Update {update} caused error {_.error}")
+
 
 def unknown(update: Update, _: CallbackContext) -> None:
     _.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
 
+
 def main() -> None:
-    
     # Create the Updater and pass it my bot's token
     updater = Updater(keys.API_KEY)
 
@@ -42,11 +46,12 @@ def main() -> None:
 
     # Start the bot
     updater.start_polling()
-    
+
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
     # start_polling() is non-blocking and will stop the bot gracefully.
     updater.idle()
+
 
 if __name__ == '__main__':
     main()
